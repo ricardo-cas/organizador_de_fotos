0a180b356493716e55bcf3678a33eb07655a2a3e
Ricardo <ricardo_cas4@hotmail.com>
2020-06-25 15:19:32 -0300

    Adicionado a funcionalidade de separacao de pastas por mês

--- organizador_de_foto.py
+++ organizador_de_foto.py
@@ -8,7 +8,7 @@ tipos = ['jpg','jpeg','JPG','JPEG']
 # Define como será a criação da estrutura de pastas para organização das imagens baseadas na data de criação da foto
 def caminho_da_pasta_com_data_de_criacao_da_foto(arquivo):
     date = data_de_criacao_da_foto(arquivo)
-    return date.strftime('%Y') + '/' + date.strftime('%Y-%m-%d')
+    return date.strftime('%Y') + '/' + date.strftime('%m') + '/' + date.strftime('%Y-%m-%d')
 
 # Consegue a data de criação da foto, acessando as propriedades do arquivo utilizando exif do Piloow
 def data_de_criacao_da_foto(arquivo):
@@ -22,6 +22,18 @@ def data_de_criacao_da_foto(arquivo):
 
     return(data)
 
+# def data_de_criacao_da_foto(arquivo):
+#     foto = Image.open(arquivo)
+#     informacao = foto.getexif()
+#     if 36867 in informacao:
+#         data = informacao[36867]
+#         data = datetime.strptime(data, '%Y:%m:%d %H:%M:%S')
+#     else:
+#         data = datetime.fromtimestamp(os.path.getatime(arquivo))
+
+#     return(data)
+
+
 # função que move as fotos para a pasta criada
 
 def move_fotos(arquivo):
@@ -29,6 +41,13 @@ def move_fotos(arquivo):
     if not os.path.exists(nova_pasta):
         os.makedirs(nova_pasta)
     shutil.move(arquivo, nova_pasta + '/' + arquivo)
+
+# def move_fotos(arquivo):
+#     nova_pasta = caminho_da_pasta_com_data_de_criacao_da_foto(arquivo)
+#     if not os.path.exists(nova_pasta):
+#         os.makedirs(nova_pasta)
+#     shutil.move(arquivo, nova_pasta + '/' + arquivo)
+
 
 def organiza():
     fotos = [
@@ -39,14 +58,14 @@ def organiza():
 
 #  Testes de resultado
 
-# print('Data de criação:')
-# print(data_de_criacao_da_foto('ny.jpg'))
+print('Data de criação:')
+print(data_de_criacao_da_foto('ny.jpg'))
 # print('Estrutura de como será a criação da pasta baseada no ano de criacao da foto:')
-# print(caminho_da_pasta_com_data_de_criacao_da_foto('ny.jpg'))
-print(organiza())
+print(caminho_da_pasta_com_data_de_criacao_da_foto('ny.jpg'))
+# print(organiza())
 
 # print('Agora vai começar a criação da pasta e mover o arquivo para a nova pasta')
-# print(move_fotos('ny.jpg'))
+print(move_fotos('ny.jpg'))
 # print(move_fotos('a.jpg'))
 # print(move_fotos('b.jpg'))
 # print(move_fotos('c.jpg'))
